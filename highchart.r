ee77649fdbef623c65620a602bdab362453b3057
daigotanaka <daigo.tanaka@gmail.com>
2016-07-25 20:12:10 -0700

    Support log scaling histogram

--- highchart.r
+++ highchart.r
@@ -3,36 +3,36 @@
 # getQ2TimelapsePlot
 # data[[]]$x: Stats
 # data[[]]$date: Date
-getQ2TimelapsePlot = function(data, names, colors, yLabel, verticalLineDate=NULL) {
+getQ2TimelapsePlot = function(data, names, colors, yLabel, verticalLineDate=NULL, timezone="UTC") {
     series = list()
+    dateFactors = list()
     for (i in 1:length(data)) {
-        dateFactor = as.factor(data[[i]]$date)
-        boxplot = boxplot(data[[i]]$x ~ dateFactor,
-                          data=data.frame(dateFactor, date[[i]]$x), plot=FALSE)
+        dateFactors[[i]] = as.factor(data[[i]]$date)
+        boxplot = boxplot(data[[i]]$x ~ dateFactors[[i]],
+                          data=data.frame(dateFactors[[i]], data[[i]]$x), plot=FALSE)
         stats = setNames(as.data.frame(boxplot$stats), nm=NULL)
 
+        # Timpstamp in miliseconds
+        unixTimestamps = 1000 * as.numeric(as.POSIXct( sort(unique(allMetrics[[1]]$date))))
+        statsMedian = rbind(setNames(unixTimestamps, nm=NULL), stats[3,])
+        statsQ2 = rbind(setNames(unixTimestamps, nm=NULL), stats[c(2, 4),])
+
         series[[2 * (i - 1) + 1]] =
-            list(name=names[i], data=stats[3,], zIndex=1,
+            list(name=names[i], data=statsMedian, zIndex=1, color=colors[i],
                  marker=list(fillColor="white", lineWidth=2, lineColor=colors[i]))
-        series[[2 * i]] = list(name="50th quartile", data=stats[c(2,4),], zIndex=0,
+        series[[2 * i]] = list(name="50th quartile", data=statsQ2, zIndex=0,
                  type="arearange", color=colors[i], lineWidth=0, linkedTo=":previous", fillOpacity=0.3)
     }
 
     chart = Highcharts$new()
+    xAxis = list(type="datetime")
     if (!is.null(verticalLineDate)){
-        verticalLinePos = which(levels(v3DateFactor)==verticalLineDate) - 1
-        chart$xAxis(
-            categories=levels(v2DateFactor), 
-            title=list(text = "Date"),
-            plotLines=list(list(
-                color="red",
-                value=verticalLinePos,
-                width=2)))
-    } else {
-        chart$xAxis(
-            categories=levels(v2DateFactor), 
-            title=list(text = "Date"))
+        date = as.POSIXlt(strptime(as.character(verticalLineDate), "%Y-%m-%d", tz=timezone))
+        xAxis[["plotLines"]] = paste("[{color: 'red',",
+                                     "value: Date.UTC(", date$year + 1900, ",", date$mon, ",", date$mday, "),",
+                                     "width: 2}]", sep="")
     }
+    chart$set(xAxis=xAxis)
     chart$yAxis(title=list(text=yLabel), min=0)
     chart$set(series=series)
     return(chart)
@@ -54,13 +54,21 @@ getBinItemList = function(data, businesses, interval=100) {
 }
 
 # getStackedHistogram
-# data[[]]$count
-# data[[]]$name
-getStackedHistogram = function(data, names, xLabel, interval=100) {
+# data[[]]$x
+getStackedHistogram = function(data, names, xLabel, interval=100, logScale=FALSE, logBase=exp(1)) {
     series = list()
     for (i in 1:length(data)){
-        histogram = hist(data[[i]]$count, breaks=seq(0, max(data[[i]]$count) + interval, interval), plot=FALSE)
-        histNames = getBinItemList(data[[i]], interval=interval)
+        x = data[[i]]$x
+        maxBin = max(data[[i]]$x)
+        actualInterval = interval
+
+        if (logScale) {
+            x = log(x + 1, base=logBase)
+            maxBin = log(maxBin + 1, base=logBase)
+            actualInterval = log(interval, base=logBase)
+        }
+        histogram = hist(x, breaks=seq(0, maxBin + actualInterval, actualInterval), plot=FALSE)
+        histNames = getBinItemList(data[[i]], interval=actualInterval)
 
         nBins = min(length(histogram$breaks), length(histogram$counts))
         bins = getValues(
@@ -73,14 +81,17 @@ getStackedHistogram = function(data, names, xLabel, interval=100) {
     chart <- Highcharts$new()
     chart$chart(type="column")
     chart$plotOptions(
-        column="{ pointPadding: 0, borderWidth: 0, groupPadding: 0, shadow: false}")
+        column="{ grouping: false, pointPadding: 0, borderWidth: 0, groupPadding: 0, shadow: false}")
     chart$xAxis(title=paste("{text: '", xLabel, "'}", sep=""))
     chart$yAxis(title="{text: 'frequency'}")
     chart$set(series=series)
     return(chart)
 }
 
-getTimelapseLinePlot = function(data, names, yLabel, timezone="UTC") {
+# getTimelapseLinePlot
+# data[[]]$x: Stats
+# data[[]]$date: Date
+getTimelapseLinePlot = function(data, names, yLabel, verticalLineDate=NULL, timezone="UTC") {
     series = list()
     for (i in 1:length(data)){
         timelapseValues = getTimelapseValues(
@@ -89,8 +100,16 @@ getTimelapseLinePlot = function(data, names, yLabel, timezone="UTC") {
         series[[i]] = list(name=names[i], data=timelapseValues)
     }
 
-    chart <- Highcharts$new()
-    chart$xAxis(type="datetime")
+
+    chart = Highcharts$new()
+    xAxis = list(type="datetime")
+    if (!is.null(verticalLineDate)){
+        date = as.POSIXlt(strptime(as.character(verticalLineDate), "%Y-%m-%d", tz=timezone))
+        xAxis[["plotLines"]] = paste("[{color: 'red',",
+                                     "value: Date.UTC(", date$year + 1900, ",", date$mon, ",", date$mday, "),",
+                                     "width: 2}]", sep="")
+    }
+    chart$set(xAxis=xAxis)
     chart$yAxis(title=paste("{text: '", yLabel, "'}", sep=""), gridLineColor="#FFFFFF")
     chart$set(series=series)
     return(chart)
