ebaf2db4e1e94a39627d7a55ba360e8bdaafd296
daigotanaka <daigo.tanaka@gmail.com>
2016-08-05 18:49:12 -0700

    Detectable difference

--- power.r
+++ power.r
@@ -15,3 +15,22 @@ getTTestSampleSize = function(x, minDetectDiffPercent, sigLevel=0.05, power=0.8,
             alternative=alternative)
     return(powerAnalysis)
 }
+
+getDetectableDiff = function(x, sampleSize, sigLevel=0.05, power=0.8, type="two.sample", alternative="two.sided") {
+    # Get the detectable difference in real scale
+    # sampleSize: Sample size in each group
+    # type = c("two.sample", "one.sample", "paired")
+    # alternative = c("two.sided", "less", "greater")
+    # Return: pwr.t.test object with n (sample size) filled
+    # Note: pooled standard deviation is approximated by the SD of x
+    require(pwr)
+    powerAnalysis =
+        pwr.t.test(
+            n=sampleSize,
+            sig.level=sigLevel,
+            power=power,
+            type=type,
+            alternative=alternative)
+    detectableDiff = powerAnalysis$d * sd(x)
+    return(detectableDiff)
+}
